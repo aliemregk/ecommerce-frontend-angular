71312800f89c749937b01ad326c0fe97e0f31c3d
aliemregk <aliemregk@outlook.com>
2023-01-05 13:13:12 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+
+  protected title: string = "Shop Application";
+
+  constructor(private readonly titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
