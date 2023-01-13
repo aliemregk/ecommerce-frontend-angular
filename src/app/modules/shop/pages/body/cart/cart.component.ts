b7586d100536ae26247e8a33d29c2371cbb39e3c
aliemregk <aliemregk@outlook.com>
2023-01-13 18:16:51 +0300

    Refactor cart page

--- src/app/modules/shop/pages/body/cart/cart.component.ts
+++ src/app/modules/shop/pages/body/cart/cart.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
 
   cartItems = [
-    {name: "product 1", quantity: 2, price: 50},
-    {name: "product 2", quantity: 1, price: 20},
-    {name: "product 3", quantity: 1, price: 15},
+    { name: "product 1", image: "https://picsum.photos/id/1/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 1, unitPrice: 20 },
+    { name: "product 2", image: "https://picsum.photos/id/2/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 4, unitPrice: 28 },
+    { name: "product 3", image: "https://picsum.photos/id/3/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 1, unitPrice: 15 },
+    { name: "product 4", image: "https://picsum.photos/id/4/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 3, unitPrice: 37 },
+    { name: "product 5", image: "https://picsum.photos/id/5/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 1, unitPrice: 65 },
+    { name: "product 6", image: "https://picsum.photos/id/6/80", description: "Lorem ipsum dolor sit amet consectetur.", quantity: 2, unitPrice: 50 }
   ]
 
   constructor() { }
