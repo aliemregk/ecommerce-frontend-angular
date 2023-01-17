0ed2dd82cc05d794adf2ebe4e1c03bc6c63b9006
aliemregk <aliemregk@outlook.com>
2023-01-17 15:01:46 +0300

    Update user-orders component

--- src/app/modules/shop/pages/body/profile/order/user-orders.component.ts
+++ src/app/modules/shop/pages/body/profile/order/user-orders.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserOrdersComponent implements OnInit {
 
+  orders = [
+    { id: 1, orderDate: "10/01/2023", status: "Delivered", deliveryDate: "14/01/2023" },
+    { id: 2, orderDate: "15/01/2023", status: "Shipping", deliveryDate: "" },
+    { id: 3, orderDate: "16/01/2023", status: "Preparing", deliveryDate: "" }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
